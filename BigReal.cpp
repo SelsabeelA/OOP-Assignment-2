941e634c54277cde5b6806c7b60257eeb27345da
Salmaabdelaziz271 <93342442+Salmaabdelaziz271@users.noreply.github.com>
2022-11-01 12:56:43 -0700

    Update BigReal.cpp

--- BigReal.cpp
+++ BigReal.cpp
@@ -2,35 +2,84 @@
 
 //Write your method functions here :)
 //constructor
-/*BigReal :: BigReal (string realNumber) {
-
+BigReal :: BigReal (string realNumber) {
+    if(realNumber[0] == '-'){
+        signNumber = '-';
+    }
+    else{
+        signNumber = '+';
+    }
     integer.setNumber(realNumber.substr(0, realNumber.find('.')));
     decimal.setNumber(realNumber.substr(realNumber.find('.') + 1, realNumber.length()));
-}*/
+}
 //operator < overloading function.
 bool BigReal :: operator < (BigReal anotherReal) {
     if (integer < anotherReal.integer)
         return true;
-    else if (integer == anotherReal.integer)
-        return decimal < anotherReal.decimal;
+    else if (integer == anotherReal.integer) {
+        string s1 = decimal.getNumber();
+        string s2 = anotherReal.decimal.getNumber();
+        if(s1.size() > s2.size()) {
+            int diff = s1.length() - s2.length();
+            for (int i = 0; i < diff; i++) {
+                s2 += '0';
+            }
+        }
+        else if(s1.size() < s2.size()) {
+            int diff = s2.length() - s1.length();
+            for (int i = 0; i < diff; i++) {
+                s1 += '0';
+            }
+        }
+        return s1 < s2;
+    }
     else
         return false;
 }
+
 //operator > overloading function.
 bool BigReal :: operator > (BigReal anotherReal) {
     if (integer > anotherReal.integer) {
         return true;
     }
     else if (integer == anotherReal.integer) {
-        return decimal > anotherReal.decimal;
+        string s1 = decimal.getNumber();
+        string s2 = anotherReal.decimal.getNumber();
+        if(s1.size() > s2.size()) {
+            int diff = s1.length() - s2.length();
+            for (int i = 0; i < diff; i++) {
+                s2 += '0';
+            }
+        }
+        else if(s1.size() < s2.size()) {
+            int diff = s2.length() - s1.length();
+            for (int i = 0; i < diff; i++) {
+                s1 += '0';
+            }
+        }
+        return s1 > s2;
     }
-    else {
+    else
         return false;
-    }
 }
 //operator == overloading function.
-bool BigReal :: operator == (BigReal anotherReal){
-    if(integer == anotherReal.integer && decimal == anotherReal.decimal){
+bool BigReal :: operator == (BigReal anotherReal)
+{
+    string s1 = decimal.getNumber();
+    string s2 = anotherReal.decimal.getNumber();
+    if(s1.size() > s2.size()) {
+        int diff = s1.length() - s2.length();
+        for (int i = 0; i < diff; i++) {
+            s2 += '0';
+        }
+    }
+    else if(s1.size() < s2.size()) {
+        int diff = s2.length() - s1.length();
+        for (int i = 0; i < diff; i++) {
+            s1 += '0';
+        }
+    }
+    if(integer == anotherReal.integer && s1 == s2){
         return true;
     }
     else{
@@ -45,7 +94,7 @@ int BigReal :: size()
 //sign function.
 int BigReal ::sign()
 {
-    if (integer.sign() == '+')
+    if (signNumber == '+')
     {
         return 1;
     }
