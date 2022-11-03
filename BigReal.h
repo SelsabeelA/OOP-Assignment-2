17125b20adb717ac402b46b83e1fb2bffd1622be
SamaAhmedS <102187124+SamaAhmedS@users.noreply.github.com>
2022-11-04 01:15:28 +0200

    Update BigReal.h
    I have added first part declaration

--- BigReal.h
+++ BigReal.h
@@ -3,8 +3,8 @@ Author1: Selsabeel Asim Ali Elbagir
 ID1: 20210714
 Author2: Salma Abdelaziz Nabieh Soliman
 ID2: 20211048
-Author3:
-ID3:
+Author3: Sama Ahmed Saeed Kalil
+ID3: 20210163
 Section: S9/S10
 TA: 
 Professor: Dr.El-Ramly
@@ -18,28 +18,34 @@ Description:
 #include <bits/stdc++.h>
 using namespace std;
 
+
 class BigReal {
 private:
-    BigDecimalInt integer;
-    BigDecimalInt decimal;
+	BigDecimalInt* integer = new BigDecimalInt("0");
+    BigDecimalInt* fraction = new BigDecimalInt("0");
     char signNumber;
 public:
-    BigReal (double realNumber = 0.0); // Default constructor
-    BigReal (string realNumber);
-    BigReal (BigDecimalInt bigInteger);
-    BigReal (const BigReal& other); // Copy constructor
-    BigReal (BigReal&& other); // Move constructor
-    BigReal& operator= (BigReal& other); // Assignment operator
-    BigReal& operator= (BigReal&& other); // Move assignment*/
+    BigReal(double realnumber = 0.0);       // Default constructor
+    BigReal(string realnumber);
+    BigReal(BigDecimalInt bigInteger);
+    BigReal(const BigReal& other);   // copy constructor
+    BigReal(BigReal&& other);       //  Move constructor
+	BigReal& operator= (BigReal& other);   // Assignment operator
+    BigReal& operator=(BigReal&& other); //  Move Assignment
     BigReal operator+ (BigReal& other);
-    BigReal operator- (BigReal& other);
-    bool operator< (BigReal anotherReal);
-    bool operator> (BigReal anotherReal);
-    bool operator== (BigReal anotherReal);
-    int size();
+    BigReal operator-(BigReal& other);
+    bool operator < (BigReal anotherReal);
+    bool operator > (BigReal anotherReal);
+    bool operator == (BigReal anotherReal);
     int sign();
+    int size();
     friend ostream& operator << (ostream& out, BigReal num);
-    friend istream& operator >> (istream& out, BigReal num);
+   // friend istream& operator >> (istream& out, BigReal num);
+    ~BigReal() {
+		delete integer;
+		delete fraction;
+	}
+
 };
 
 #endif //UNTITLED2_BIGREAL_H
